dc9e024ae3e48cdfe70691c43c3f5d3f8c6ef510
stgleb <glebstepanov1992@gmail.com>
2015-05-18 22:42:18 +0300

    env tests continuing

--- test/test_openstack.py
+++ test/test_openstack.py
@@ -1,10 +1,11 @@
 import unittest
 from cinder_api import CinderClient
-
 from cinderclient.v1 import Client
+from novaclient.v2 import client as novaclient
 
 
 class TestCinderClientOpenstack(unittest.TestCase):
+    @classmethod
     def setUpClass(cls):
         username = "admin"
         password = "admin"
@@ -14,18 +15,36 @@ class TestCinderClientOpenstack(unittest.TestCase):
         cinder = Client(username, password,
                         tenant, auth_url,
                         service_type="volume")
-        cls.client = CinderClient(cinder)
+        cls.cinder_client = cinder
+        cls.cinder = CinderClient(cinder)
+        cls.nova = novaclient.Client(username,
+                                     password,
+                                     tenant,
+                                     auth_url)
 
     def test_create_delete(self):
-        vol_id = self.client.create_volume('test_volume', size=1)
+        vol_id = self.cinder.create_volume('test_volume', size=1)
         assert len(vol_id) == 36
-
-
-    def test_delete(self):
+        res = self.cinder.delete_volume(volume_id=vol_id)
+        assert res == [vol_id]
         pass
 
     def test_attach_detach(self):
-        pass
+        flavors = self.nova.flavors.list()
+        images = self.nova.images.list()
+        assert len(images) > 0
+        image = images[0]
+        flavor = flavors[0]
+        server = self.nova.servers.create(name="test_server",
+                                          image=image,
+                                          flavor=flavor)
+        volume = self.cinder_client.volumes.create(display_name="test_volume",
+                                                   size=1)
+        self.cinder.attach_volume(volume, server.id, "/dev/vda")
+        self.cinder_client.volumes.delete(volume=volume)
 
     def test_find_vm(self):
         pass
+
+if __name__ == '__main__':
+    unittest.main()
